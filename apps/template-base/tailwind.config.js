a6b16da408e455a3da3630f1108b47aa6bb4206a
Eduardo González Victorino <eduardogonzalezvictorino@MacBook-Air-de-Eduardo.local>
2026-02-27 20:52:21 -0600

    feat: cambio nombre del repo

--- apps/template-base/tailwind.config.js
+++ apps/template-base/tailwind.config.js
@@ -1,11 +1,10 @@
-import pymesPreset from '@pymes/ui-components/tailwind.preset';
+import bipPreset from '@bip/ui-components/tailwind.preset';
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     './index.html',
     './src/**/*.{js,ts,jsx,tsx}',
-    './node_modules/@pymes/ui-components/dist/**/*.js',
   ],
-  presets: [pymesPreset],
+  presets: [bipPreset],
 };
--- packages/ui-components/tailwind.preset.js
+++ packages/ui-components/tailwind.preset.js
@@ -1,5 +1,12 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 /** @type {import('tailwindcss').Config} */
 export default {
+  content: [join(__dirname, './dist/**/*.js')],
   theme: {
     extend: {
       colors: {
